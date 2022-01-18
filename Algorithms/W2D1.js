43e998d8f631987ac2fdad60213335054cb4674e
Inxie <moniqueprousseau@icloud.com>
2022-01-18 17:11:26 -0600

    initial commit of week 2 day 1 algorithm

---
+++ Algorithms/W2D1.js
@@ -0,0 +1,21 @@
+function strToWordArr(str) {
+    let result = [];
+    let word = "";
+    for (let i = 0; i < str.length; i++) {
+        // console.log(str[i]);
+        if (str[i] === " ") {
+            // this finds the first space
+            while (str[i] === " ") {
+                // skips through any following spaces
+                i++;
+            }
+            if (word != "") {
+                result.push(word); // add the word that preceded the space
+                word = ""; // reset word
+            }
+        }
+        word += str[i];
+    }
+    return result;
+}
+console.log(strToWordArr("  Welcome  to week    two!")) //["Welcome", "to", "week", "two!"]
