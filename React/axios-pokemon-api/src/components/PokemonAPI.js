95ce36bb97537081d71cee53d721daa7653b84ff
Inxie <moniqueprousseau@icloud.com>
2022-01-23 10:38:42 -0600

    initial commit of axios pokemon api react app

---
+++ React/axios-pokemon-api/src/components/PokemonAPI.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const PokemonAPI = () => {
+
+    let [allPokemon, setAllPokemon] = useState([])
+
+    const getPokemon = () => {
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=1000")
+        .then(response => {
+            setAllPokemon(response.data)
+        })
+
+        .catch(err => {
+            console.log("ERROR!", err)
+        })
+    }
+        
+    return (
+        <div>
+            <button onClick={getPokemon}>Fetch Pokemon</button>
+            {
+                allPokemon.map((pokeObj, i) => {
+                    return (
+                        <div key = {i}>
+                            <p>{pokeObj.name}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default PokemonAPI;
